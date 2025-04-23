bdd27ca51ceee01347a454e26374f70e1ef18e54
Shinde-Anushka <anushka18.shinde@gmail.com>
2025-04-24 00:41:18 +0530

    Create Assignment-13.cpp

---
+++ Assignment-13.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+int rollCounter = 1;
+
+class Marks {
+protected:
+    int rollNumber;
+    string name;
+public:
+    Marks() {
+        rollNumber = rollCounter++;
+    }
+    void inputName() {
+        cout << "Enter name: ";
+        cin >> name;
+    }
+    void displayBasicInfo() {
+        cout << "Roll No: " << rollNumber << ", Name: " << name;
+    }
+};
+
+class Physics : virtual public Marks {
+protected:
+    float physicsMarks;
+public:
+    void inputPhysics() {
+        cout << "Enter Physics marks: ";
+        cin >> physicsMarks;
+    }
+};
+
+class Chemistry : virtual public Marks {
+protected:
+    float chemistryMarks;
+public:
+    void inputChemistry() {
+        cout << "Enter Chemistry marks: ";
+        cin >> chemistryMarks;
+    }
+};
+
+class Mathematics : public Physics, public Chemistry {
+protected:
+    float mathMarks;
+    float totalMarks;
+public:
+    void inputAllMarks() {
+        inputName();
+        inputPhysics();
+        inputChemistry();
+        cout << "Enter Mathematics marks: ";
+        cin >> mathMarks;
+        totalMarks = physicsMarks + chemistryMarks + mathMarks;
+    }
+
+    void displayAllMarks() {
+        displayBasicInfo();
+        cout << ", Physics: " << physicsMarks
+             << ", Chemistry: " << chemistryMarks
+             << ", Math: " << mathMarks
+             << ", Total: " << totalMarks << endl;
+    }
+
+    float getTotalMarks() {
+        return totalMarks;
+    }
+};
+
+int main() {
+    int num;
+    cout << "Enter number of students: ";
+    cin >> num;
+
+    Mathematics students[100];  // Max 100 students
+
+    float classTotal = 0;
+
+    for (int i = 0; i < num; ++i) {
+        cout << "\nEnter details for student " << (i + 1) << ":\n";
+        students[i].inputAllMarks();
+        classTotal += students[i].getTotalMarks();
+    }
+
+    cout << "\n--- Students Marks Summary ---\n";
+    for (int i = 0; i < num; ++i) {
+        students[i].displayAllMarks();
+    }
+
+    float average = classTotal / num;
+    cout << "\nAverage marks of the class: " << average << endl;
+
+    return 0;
+}
