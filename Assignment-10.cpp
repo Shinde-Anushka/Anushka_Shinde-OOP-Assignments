3e55013f9618f2779c0f175a2328c500760cf3b1
Shinde-Anushka <anushka18.shinde@gmail.com>
2025-04-24 01:06:20 +0530

    Update Assignment-10.cpp

--- Assignment-10.cpp
+++ Assignment-10.cpp
@@ -1,3 +1,15 @@
+// 10. The Employee class
+// 1. Write the code that declares a class called Employee with these data members: age, yearsOfService,
+// and Salary.
+// 2. Rewrite the Employee class to make the data members private, and provide public accessor methods
+// to get and set each of the data members.
+// 3. Write a program with the Employee class that makes two Employees; sets their age,
+// YearsOfService, and Salary; and prints their values.
+// 4. Continuing from Exercise 3, provide a method of Employee that reports how many thousands of
+// dollars the employee earns, rounded to the nearest 1,000.
+// 5. Change the Employee class so that you can initialize age, YearsOfService, and Salary when you
+// create the employee.
+
 #include <iostream>
 using namespace std;
 
