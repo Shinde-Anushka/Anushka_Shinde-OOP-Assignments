481f57f82877e308700df6d8c4e29a7238753ca9
Shinde-Anushka <anushka18.shinde@gmail.com>
2025-04-24 01:00:03 +0530

    Update Assignment-3.cpp

--- Assignment-3.cpp
+++ Assignment-3.cpp
@@ -1,3 +1,8 @@
+// Q3 An educational institution wishes to maintain a database of its employees. The database is divided into
+//  several classes whose hierarchical relationships are shown in following figure. The figure also shows
+//  the minimum information required for each class. Specify all classes and define functions to create the
+//  database and retrieve individual details as and when needed
+
 #include <iostream>
 Using namespace std;
 Class StaffMember {
