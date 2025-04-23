68fbdc7bab18f807a8bd9a48472ed65f1de1553a
Shinde-Anushka <anushka18.shinde@gmail.com>
2025-04-24 01:10:23 +0530

    Update Assignment-15.cpp

--- Assignment-15.cpp
+++ Assignment-15.cpp
@@ -1,3 +1,10 @@
+// Q15. Create a class named Shape with a function that prints "This is a shape". Create another class
+// named Polygon inheriting the Shape class with the same function that prints "Polygon is a shape".
+// Create two other classes named Rectangle and Triangle having the same function which prints
+// "Rectangle is a polygon" and "Triangle is a polygon" respectively. Again, make another class named
+// Square having the same function which prints "Square is a rectangle".
+// Now, try calling the function by the object of each of these classes
+
 #include <iostream>
 #include <string>
 using namespace std;
